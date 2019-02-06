a711b40c9ca7e3aee18dd9019d352be5e318b4b6
RaphaelOlivier <raphael.franck.olivier@gmail.com>
2019-02-06 17:28:35 -0500

    gitignore

--- recitation-4/code/paths.py
+++
@@ -1,9 +0,0 @@
-data_prefix = "../data/"
-train_data = data_prefix+"train_data.npy"
-train_labels = data_prefix+"train_labels.npy"
-valid_data = data_prefix+"valid_data.npy"
-valid_labels = data_prefix+"valid_labels.npy"
-test_data = data_prefix+"test_data.npy"
-test_labels = data_prefix+"test_labels.npy"
-
-model = "../model/model_dict.pt"
